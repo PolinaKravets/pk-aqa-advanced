1eacd6fad81d3b0a550e6724e8711f453e3bf9dd
Polina Kravets <you@example.com>
2024-09-03 20:29:55 +0300

    deleted_files

--- jsBasics/task2.js
+++
@@ -1,5 +0,0 @@
-let qaOne = 'Polina'
-let qaTwo = 'Alex'
-
-console.log('Hello ' + qaOne + ' and '+ qaTwo + '!')
-console.log(`Hello ${qaOne} and ${qaTwo}!`)
--- jsBasics/task3.js
+++
@@ -1,4 +0,0 @@
-let personAge = 15
-let validateAge 
-console.log(personAge)
-console.log(validateAge = personAge >= 16)
