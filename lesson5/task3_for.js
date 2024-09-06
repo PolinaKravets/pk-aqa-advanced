dc7b2767481a5ae2ca1f73aad83a33e2a16607dc
Polina Kravets <you@example.com>
2024-09-06 17:37:25 +0300

    hw_lesson5

---
+++ lesson5/task3_for.js
@@ -0,0 +1,6 @@
+const number = 3
+
+for (let i = 1; i <= 10; i++) {
+    let result = number * i
+    console.log(`${number} x ${i} = ${result}`)
+}
---
+++ lesson5/task3_while.js
@@ -0,0 +1,8 @@
+const number = 4
+let count = 1
+
+while (count <= 10) {
+    let result = number * count
+    console.log(`${number} x ${count} = ${result}`)
+    count++
+}
