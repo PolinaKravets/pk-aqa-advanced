420608910e579220c78dad9326125cb9dc8882ef
Polina Kravets <you@example.com>
2024-09-06 17:52:59 +0300

    fix_condition

--- lesson5/task1.js
+++ lesson5/task1.js
@@ -1,22 +1,22 @@
-let averageGrade = 78
+let averageGrade = 70
 
 if( averageGrade < 60)
     {
     console.log('Незадовільно')
     }
-else if (averageGrade >= 60 && averageGrade <= 70)
+else if ( averageGrade <= 70)
     {
         console.log('Задовільно')
     }
-else if (averageGrade > 70 && averageGrade <= 80)
+else if (averageGrade <= 80)
     {
         console.log('Добре')
     }   
-else if (averageGrade > 80 && averageGrade <= 90)
+else if (averageGrade <= 90)
     {
         console.log('Дуже добре')
     }  
-else if (averageGrade > 90 && averageGrade <= 100)
+else if (averageGrade <= 100)
     {
         console.log('Відмінно')
     }
