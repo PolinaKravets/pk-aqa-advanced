0af3d18d3743f1ec8e3610a59a5e1ce1964f90b7
Polina Kravets <you@example.com>
2024-09-20 16:57:25 +0300

    hw_lesson9

---
+++ lesson9/task5.js
@@ -0,0 +1,26 @@
+// Створіть масив об'єктів users де обєкти мають довільні властивості (наприклад, name, email, age, тощо).
+// Використовуючи цикл for...of, переберіть всі елементи масиву та виведіть їхні значення в консоль.
+// Зробіть деструктуризацію в циклі
+
+
+const users = [
+    {
+    name: 'John Stermak', 
+    email: 'joster@gmail.com',
+    age: 36,
+    position: 'PO'
+},
+{
+    name: 'Abby Maylor',
+    email:'abma@gmail.com',
+    age: 27,
+    position: 'PM'
+}
+]
+
+for (const {name, email, age, position} of users){
+console.log(name, email,age,position)
+}
+
+
+    
